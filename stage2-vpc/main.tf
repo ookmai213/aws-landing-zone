80e26af0ec2b8f5f29a2c3963136f6fa27c5a779
Aniketh B <ookmai213@gmail.com>
2026-09-23 00:15:20 +0530

    Stage 1 and Stage 2: Terraform fundamentals + AWS VPC, subnets, security group, RHEL EC2 instance

---
+++ stage2-vpc/main.tf
@@ -0,0 +1,144 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "project1-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "project1-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block               = "10.0.1.0/24"
+  availability_zone        = "ap-south-1a"
+  map_public_ip_on_launch  = true
+
+  tags = {
+    Name = "project1-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "project1-private-subnet"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "project1-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "public_sg" {
+  name        = "project1-public-sg"
+  description = "Allow SSH from my IP only, and HTTP/HTTPS from anywhere"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["122.171.20.175/32"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "project1-public-sg"
+  }
+}
+
+data "aws_ami" "rhel" {
+  most_recent = true
+  owners      = ["309956199498"]  # Red Hat's official AWS account
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9*HVM*x86_64*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "project1_key" {
+  key_name   = "project1-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKY1qqwycBzCFbWiDbk8ee6wjAj2NMJt5OP6I67b3g9X project1-aws-key"
+}
+
+resource "aws_instance" "rhel_server" {
+  ami                    = data.aws_ami.rhel.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  key_name               = aws_key_pair.project1_key.key_name
+
+  tags = {
+    Name = "project1-rhel-server"
+  }
+}
+
+output "instance_public_ip" {
+  value       = aws_instance.rhel_server.public_ip
+  description = "Public IP of the RHEL server"
+}
+
+output "instance_id" {
+  value       = aws_instance.rhel_server.id
+  description = "Instance ID of the RHEL server"
+}
